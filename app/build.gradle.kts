62ed00e5870e214b360a497ebcaa59cd10718e71
blackberrythangbv-bit <blackberrythangbv@gmail.com>
2026-09-13 15:10:05 +0700

    Bump app version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "vn.viettel.caobang.kpitammi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.2.1"
     }
 
     compileOptions {
